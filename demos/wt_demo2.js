c4bf1cac29fd26fd4bb3756a108d4229fab57929
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2018-01-06 21:28:02 -0300

    Demo 2 api call

---
+++ demos/wt_demo2.js
@@ -0,0 +1,16 @@
+var request = require('request-promise');
+
+module.exports = function(ctx, cb) {
+  var opts = {
+    url: 'https://api.mercadolibre.com/items/MLA606927509',
+    json: true
+  };
+  request.get(opts)
+    .then(item => {
+      cb(null, item);    
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
